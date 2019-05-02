ee4cce4393bcc06711ac2c36f2fdbdf90643cccd
sangbum <kl9151@naver.com>
2019-05-02 18:10:37 +0900

    프로젝트 세팅 변경

---
+++ CustomPlayer/Extensions/Extenstions.swift
@@ -0,0 +1,137 @@
+//
+//  Extenstions.swift
+//  MPEG-4Parser
+//
+//  Created by USER on 26/04/2019.
+//  Copyright © 2019 bumslap. All rights reserved.
+//
+
+import Foundation
+
+extension Data {
+    var convertToInt: Int {
+        var uIntArray: [UInt8] = []
+        self.forEach {
+            uIntArray.append($0)
+        }
+        return uIntArray.tohexNumbers.toDecimalValue
+    }
+    
+    func slice(in sizes: [Int]) -> [Data] {
+        
+        var offset = 0
+        var slicedData: [Data] = []
+        
+        for size in sizes {
+            if self.count > (offset + size) {
+                slicedData.append(self.subdata(in: offset..<(offset + size)))
+                offset += size
+            } else {
+                break
+            }
+        }
+        return slicedData
+    }
+    
+    var convertToString: String {
+       
+        guard let convertedString = String(data: self, encoding: .utf8) else {
+            return ""
+        }
+        return convertedString
+    }
+}
+
+extension String {
+    var convertHexStringToData: Data {
+        var hexString = self
+        var data = Data()
+        while hexString.count > 0 {
+            let subIndex = hexString.index(hexString.startIndex, offsetBy: 2)
+            let slicedString = String(hexString[..<subIndex])
+            hexString = String(hexString[subIndex...])
+            var tempStorageForUInt8: UInt32 = 0
+            Scanner(string: slicedString).scanHexInt32(&tempStorageForUInt8)
+            var convertedNumber = UInt8(tempStorageForUInt8)
+            data.append(&convertedNumber, count: 1)
+        }
+        return data
+    }
+}
+
+extension Array where Element == UInt8 {
+    mutating func flush() {
+        self.removeAll()
+    }
+    
+    var tohexNumbers: [String] {
+        let hexNumbers = self.map {
+            $0.toHexNumber
+        }
+        return hexNumbers
+    }
+}
+
+extension UInt8 {
+    var toHexNumber: String {
+        return String(format:"%02X", self)
+    }
+}
+
+extension InputStream {
+    func readByHexNumber(_ buffer: UnsafeMutablePointer<UInt8>, maxLength len: Int) -> Int {
+        
+        return self.read(buffer, maxLength: len)
+    }
+}
+
+extension Array where Element == String {
+    var mergeToString: String {
+        let mergedString = self.joined()
+        return mergedString
+    }
+    
+    var toDecimalValue: Int {
+        guard let hexNumber = Int(self.mergeToString, radix: 16) else {
+            return 0
+        }
+        return hexNumber
+    }
+}
+
+extension Array where Element == Data {
+    var toUInt8Array: [UInt8] {
+        var array: [UInt8] = []
+        self.forEach {
+            array.append(contentsOf: $0)
+        }
+        return array
+    }
+}
+
+extension Container {
+
+    var isParent: Bool {
+        if type == .moov || type == .trak || type == .mdia || type == .minf
+            || type == .dinf || type == .stbl || type == .edts || type == .esds
+            || type == .mp4a || type == .udta {
+            return true
+        } else {
+            return false
+        }
+    }
+}
+
+extension FileHandle {
+    func hasMoreData() -> Bool {
+        let position = offsetInFile
+        let length = seekToEndOfFile()
+        if position < length {
+            seek(toFileOffset: position)
+            return true
+        }
+        else {
+            return false
+        }
+    }
+}
---
+++ CustomPlayer/FileReader/MediaFileReader.swift
@@ -0,0 +1,238 @@
+//
+//  MediaFileReader.swift
+//  MPEG-4Parser
+//
+//  Created by bumslap on 29/04/2019.
+//  Copyright © 2019 bumslap. All rights reserved.
+//
+
+import Foundation
+
+class MediaFileReader {
+    let fileReader: FileStreamReadable
+    let typeOfContainer: FileContainerType
+    let containerPool: ContainerPool = ContainerPool()
+    let root: RootType
+    
+    private let headerSize = 8
+     private var fileOffset = 0
+    
+    init(fileReader: FileStreamReadable, type: FileContainerType) {
+        self.fileReader = fileReader
+        self.typeOfContainer = type
+        let root = RootType()
+        root.size = 8316725 + 8
+        self.root = root
+    }
+    
+    private func readHeader(completion: @escaping ((Int, String))->()) {
+        fileReader.read(length: 8) {(data) in
+            let result = self.converToHeaderInfo(data: data)
+            completion(result)
+
+        }
+    }
+    
+    private func converToHeaderInfo(data: Data) -> (Int, String) {
+        let sizeData = data.subdata(in: 0..<4)
+        let size = sizeData.convertToInt
+        guard let decodedHeaderName = String(data: data.subdata(in: 4..<8),
+                                             encoding: .utf8) else {
+                                                return (0,"")
+        }
+        return (size, decodedHeaderName)
+    }
+    
+    func decodeFile(type: FileContainerType) {
+        //TODO filetype 에 따라 다른 디코딩 방식제공해야함
+        var containers: [HalfContainer] = []
+        containers = decode(root: self.root)
+        while let item = containers.first {
+            containers.remove(at: 0)
+            let parentContainers = decode(root: item)
+            containers.append(contentsOf: parentContainers)
+        }
+        root.parse()
+        print(root.moov.traks[0].mdia.minf.stbl.stsd.avc1.avcc.pictureParameterSet)
+        print("________________________")
+    }
+    
+    private func decode(root: HalfContainer) -> [HalfContainer] {
+        var containers: [HalfContainer] = []
+        var currentRootContainer: HalfContainer = root
+        var currentOffset = currentRootContainer.offset
+        var isfinished: Bool = false
+        fileReader.seek(offset: currentOffset)
+        var sizeOfChildren = 0
+       repeat {
+            readHeader() { [weak self] (headerData) in
+                guard let self = self else { return }
+                let size = headerData.0
+                let headerName = headerData.1
+                sizeOfChildren += size
+                if (sizeOfChildren) == (currentRootContainer.size - 8)  { isfinished = true }
+                currentOffset = self.fileReader.currentOffset()
+                    self.fileReader.read(length: size - self.headerSize) { (data) in
+                        do {
+                                let typeOfContainer = try self.containerPool.pullOutContainer(with: headerName)
+                                var box = self.containerPool.pullOutFileTypeContainer(with: typeOfContainer)
+                                box.size = size
+                            
+                                if box.isParent {
+                                    guard var castedBox = box as? HalfContainer else { return }
+                                    castedBox.offset = currentOffset//self.fileReader.currentOffset() - UInt64(size + self.headerSize)
+                                    containers.append(castedBox)
+                                } else {
+                                    box.data = data[0..<(size - self.headerSize)]
+                                }
+                                currentRootContainer.children.append(box)
+                            
+                        } catch {
+                            assertionFailure("initialization box failed")
+                            return
+                        }
+                }
+            }
+        }  while !isfinished
+        return containers
+    }
+
+
+    
+    func makeTracks() -> [Track] {
+        let numberOftracks = root.moov.traks.count
+        var tracks: [Track] = []
+        
+        for (index, trak) in root.moov.traks.enumerated() {
+            let trackItem = Track()
+            
+            let numberOfChunks = trak.mdia.minf.stbl.stco.entryCount
+            var chunks: [Chunk] = [Chunk](repeating: Chunk(), count: numberOfChunks)
+            
+            let numberOfsamples = trak.mdia.minf.stbl.stsz.sampleCount
+            var samples: [Sample] = [Sample](repeating: Sample(), count: numberOfsamples)
+            let sampleSize = trak.mdia.minf.stbl.stsz.samplesSize
+            
+            for (index,sampleOffset) in trak.mdia.minf.stbl.stco.chunkOffsets.enumerated() {
+                chunks[index].offset = sampleOffset
+                
+            }
+            
+            var sampleNumber = 1
+            let numberOfchunkIndexes = trak.mdia.minf.stbl.stsc.entryCount
+            for index in 0..<numberOfchunkIndexes {
+                
+                let lastIndex = numberOfchunkIndexes - 1
+                let firstChunkId = trak.mdia.minf.stbl.stsc.firstChunks[index]
+                let samples = trak.mdia.minf.stbl.stsc.samplesPerChunks[index]
+                
+                let nextChunkId = index != lastIndex ?
+                    trak.mdia.minf.stbl.stsc.firstChunks[index+1] : chunks.count
+            
+                let sampleDescriptionIndex = trak.mdia.minf.stbl.stsc.sampleDescriptionIndexes[index]
+                
+                var startChunkId = firstChunkId - 1
+                while startChunkId < nextChunkId {
+                    
+                    chunks[startChunkId].sampleCount = samples
+                    chunks[startChunkId].sampleDescriptionIndex = sampleDescriptionIndex
+                    chunks[startChunkId].startSample = sampleNumber
+                    
+                    sampleNumber += samples
+                    startChunkId += 1
+                }
+            }
+            
+            for index in 0..<numberOfsamples {
+                 samples[index].size = sampleSize == 0 ?
+                    trak.mdia.minf.stbl.stsz.entrySizes[index] : sampleSize
+                //print("size\( samples[index].size)")
+            }
+            
+            var sampleId = 0
+            
+            for index in 0..<numberOfChunks {
+                var chunkOffset = chunks[index].offset
+                let samplesPerChunk = chunks[index].sampleCount
+                for _ in 0..<samplesPerChunk {
+                    samples[sampleId].offset = chunkOffset
+                    chunkOffset += samples[sampleId].size
+                    sampleId += 1
+                }
+            }
+            
+            sampleId = 0
+            
+            var sampleTime = 0
+            for index in 0..<trak.mdia.minf.stbl.stts.entryCount {
+                let sampleDuration = trak.mdia.minf.stbl.stts.sampleDeltas[index]
+                for _ in 0..<trak.mdia.minf.stbl.stts.sampleCounts[index] {
+                    samples[sampleId].startTime = sampleTime
+                    samples[sampleId].duration = sampleDuration
+                    sampleTime += sampleDuration
+                    sampleId += 1
+                }
+            }
+            
+            if !trak.mdia.minf.stbl.ctts.data.isEmpty {
+                sampleId = 0
+                for index in 0..<trak.mdia.minf.stbl.ctts.entryCount {
+                    let numberOfSamples = trak.mdia.minf.stbl.ctts.sampleCounts[index]
+                    let sampleOffset = trak.mdia.minf.stbl.ctts.sampleOffsets[index]
+                    for _ in 0..<numberOfSamples {
+                        samples[sampleId].compositionTimeOffset = sampleOffset
+                        sampleId += 1
+                    }
+                }
+            }
+            
+            if trak.mdia.hdlr.handlerType == "vide" {
+                let avcC = trak.mdia.minf.stbl.stsd.avc1.avcc
+                trackItem.sequenceParameters = avcC.sequenceParameters
+                trackItem.sequenceParameterSet = avcC.sequenceParameterSet
+                print(avcC.pictureParams)
+                trackItem.pictureParams = avcC.pictureParams
+                trackItem.pictureParameterSet = avcC.pictureParameterSet
+            }
+            
+            trackItem.chunks = chunks
+            trackItem.samples = samples
+            tracks.append(trackItem)
+        }
+        return tracks
+    }
+    
+    
+ 
+    
+    func chunkToStream() {
+        //print(root.moov.traks[0].chunks)
+        let chunks = root.moov.traks[0].chunks
+        let samples = root.moov.traks[0].samples
+        print(chunks[0].offset)
+        print(samples[0].offset)
+        print(root.moov.mvhd.creationDate)
+    }
+}
+enum FileContainerType {
+    case mp4
+}
+
+
+class TrackItem {
+    var sampleDuration: Int = 0
+    var startTime: Int = 0
+    var size: Int = 0
+}
+
+class Track {
+    var chunks: [Chunk] = []
+    var samples: [Sample] = []
+    
+    var sequenceParameterSet: Data = Data()
+    var sequenceParameters: [Data] = []
+    
+    var pictureParameterSet: Data = Data()
+    var pictureParams: [Data] = []
+    
+}
