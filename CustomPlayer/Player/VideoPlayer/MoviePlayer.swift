db543455dc16507b8fbeb3f9cff04fa0dfd02608
sangbum <kl9151@naver.com>
2019-05-28 18:46:30 +0900

    FIX: AUD 내부 다수 프레임 있을 시 꺠짐현상 해결

--- CustomPlayer/Player/VideoPlayer/MoviePlayer.swift
+++ CustomPlayer/Player/VideoPlayer/MoviePlayer.swift
@@ -117,8 +117,6 @@ class MoviePlayer: NSObject {
                 isAudioReady = true
             }
         }
-        print(isAudioReady)
-        print(isVideoReady)
         isPlayable = isAudioReady && isVideoReady
         if isPlayable { queue.startRunning() }
         
@@ -197,8 +195,8 @@ class MoviePlayer: NSObject {
 //                            }
 //                        }
                         
-                        m3u8Player.parseMediaPlaylist(list: masterPlaylist.mediaPlaylists[0]) {
-                            self.currentPlayingItemIndex = ListIndex(gear: 0, index: 0)
+                        m3u8Player.parseMediaPlaylist(list: masterPlaylist.mediaPlaylists[2]) {
+                            self.currentPlayingItemIndex = ListIndex(gear: 2, index: 3)
                             self.masterPlaylist = masterPlaylist
                             guard let currentPlaylist = self.currentPlayingItemIndex else { return }
                             guard let tempPlaylistPath = masterPlaylist
@@ -242,19 +240,15 @@ class MoviePlayer: NSObject {
                                 var audioTimings = [CMSampleTimingInfo]()
                                 var pts: [Int] = []
                                 var datas = [Data]()
-                                var count = 0
                                 tsStreams.forEach {
                                     switch $0.type {
                                     case .video:
                                         videoDataArray.append(contentsOf: $0.actualData)
-                                        videoTimings.append(CMSampleTimingInfo(duration: CMTime(value: 3000, timescale: 90000),
-                                                                               presentationTimeStamp: CMTime(value: CMTimeValue($0.pts), timescale: 90000),
+                                        videoTimings.append(CMSampleTimingInfo(duration: CMTime(value: 24, timescale: 1000),
+                                                                               presentationTimeStamp: CMTime(value: CMTimeValue($0.pts), timescale: 1000),
                                                                                decodeTimeStamp: CMTime(value: CMTimeValue($0.dts), timescale: 1000)))
                                       
                                     case .audio:
-                                        
-                                        print(count)
-                                        count += 1
                                         pts.append($0.pts)
                                         datas.append(Data($0.actualData))
                                         audioDataArray.append(contentsOf: $0.actualData)
@@ -272,7 +266,6 @@ class MoviePlayer: NSObject {
                                 if !audioDataArray.isEmpty {
                                     let dataPackage = DataPackage(presentationTimestamp: pts, dataStorage: datas)
                                     self.audioDecoder = AAC_ADTSDecoder(track: Track(type: .audio), dataPackage: dataPackage)
-                                    self.audioDecoder?.isAdts = true
                                     self.audioDecoder?.audioDelegate = self
                                     self.audioDecoder?.decodeTrack(timeScale: 44100)
                                 }
